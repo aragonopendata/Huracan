a80c431bef53a7cab13aa8e29f117e4eb51e4eb8
Sergio Rubio <rubiojr@frameos.org>
2014-09-26 20:24:48 +0200

    Added /track/:id/photos endpoint

--- ws/app.rb
+++ ws/app.rb
@@ -1,7 +1,13 @@
+require 'sinatra'
+require 'open-uri'
+require 'json'
+#require_relative 'gpx'
+
 configure do
   enable :sessions
   set :json_encoder, :to_json
   set :erb, :layout => :layout
+  set :base_url, "http://senderos.turismodearagon.com/visorprames/"
 end
 
 before do
@@ -13,4 +19,11 @@ end
 options "*" do
   response.headers["Allow"] = "HEAD,GET,PUT,DELETE,OPTIONS,POST"
   response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+end
+
+# return the waypoints with photos in a given track id
+get '/track/:id/photos' do
+  id = params[:id]
+  wps = JSON.parse(open("#{settings.base_url}/get_waypoints.php?id=#{id}").read)
+  wps.find_all{ |wp| wp['photo'] != nil }.to_json
 end
