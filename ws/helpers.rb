fa67094d06ce5642312811a98c42eedacb6e2925
Sergio Rubio <rubiojr@frameos.org>
2014-09-27 01:09:39 +0200

    Added altitude_profile_png helper

--- ws/helpers.rb
+++ ws/helpers.rb
@@ -1,4 +1,6 @@
 helpers do
+  require 'gruff'
+
   def get_gpx(id)
     gpx_cache = settings.cache_dir + "/gpx"
     gpx_file = gpx_cache + "/#{id}.gpx"
@@ -13,4 +15,50 @@ helpers do
 
     gpx_file
   end
+
+  def x_ticks(data, num = 6)
+
+    ticks = []
+    h = {}
+
+    num.times { |n| ticks << (data.last/num).round*n }
+    ticks << data[-2].round
+
+    ticks.reverse.each { |t| h[t] = t.to_s }
+    h
+  end
+
+  def altitude_profile_png(csv, id, size="200x100")
+    img_cache = settings.cache_dir + "/img"
+    img_file = img_cache + "/#{id}_#{size}.png"
+
+    FileUtils.mkdir_p img_cache
+
+    return img_file if File.exist?(img_file)
+
+    g = Gruff::Line.new(size)
+
+    g.title = 'Altitude Profile'
+    g.theme = Gruff::Themes::GREYSCALE
+    g.line_width = 3
+
+    distances = []
+    altitudes = []
+
+    csv.each_line do |l|
+      data = l.split(',').map { |line| line.strip.chomp }
+
+      distances << data[0].to_f
+      altitudes << data[1].to_f
+    end
+
+    g.dataxy('Altitude/Distance', distances, altitudes)
+    g.labels = x_ticks(distances)
+
+    g.write(img_file)
+
+    img_file
+
+  end
+
 end
