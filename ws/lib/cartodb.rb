c5eb45a535d95c3478bcf61d835c936b3e9cbcaf
Luis Bosque <luisico@gmail.com>
2014-09-27 11:48:49 +0200

    Refactor in CartoDB queries

--- ws/lib/cartodb.rb
+++ ws/lib/cartodb.rb
@@ -3,21 +3,31 @@ require 'json'
 
 class CartoDB
 
-  CARTODB_DOMAIN='jacathon-huracan.cartodb.com'
-  SQL_API_PREFIX="http://#{CARTODB_DOMAIN}/api/v2/sql"
+  CARTODB_DOMAIN = 'jacathon-huracan.cartodb.com'
+  SQL_API_PREFIX = "http://#{CARTODB_DOMAIN}/api/v2/sql"
+  TRACK_COLUMNS = [
+                    "name", "_desc", "route_type", "route_classes", 
+                    "horizontal_distance", "positive_difference" ,
+                    "negative_difference", "aproximated_time",
+                    "fid AS track_id"
+                  ]
 
   def self.get_track(track_id)
-    sql = "SELECT * FROM tracks where fid=#{track_id}"
+    sql_fields = TRACK_COLUMNS.join(', ')
+    sql = "SELECT #{sql_fields} FROM tracks where fid=#{track_id}"
     send_query(sql)[0]
   end
 
   def self.get_tracks(options = {})
+    sql_fields = TRACK_COLUMNS.map {|c| "t.#{c}"}
+
     options['term'] ||= ''
 
     geo = false
     if !options['lat'].nil? && !options['lon'].nil?
-      distance_column = ", ST_Distance(tp.the_geom::geography, ST_SetSRID(ST_Point(#{options['lat']}, #{options['lon']}),4326)::geography) AS distance"
+      distance_column = "ST_Distance(tp.the_geom::geography, ST_SetSRID(ST_Point(#{options['lat']}, #{options['lon']}),4326)::geography) AS distance"
       sql_order = " ORDER BY distance ASC"
+      sql_fields << distance_column
       geo = true
     end
 
@@ -29,13 +39,14 @@ class CartoDB
     limit_sql = options['limit'].nil? ? " LIMIT 10" : " LIMIT #{options['limit']}"
 
     sql = "
-      SELECT t.fid AS track_id, tp.cartodb_id, tp.ele, tp.the_geom, tp.the_geom_webmercator, t.name FROM track_points tp, tracks t #{distance_column if geo}
+      SELECT #{sql_fields.join(', ')} FROM track_points tp, tracks t
       WHERE track_seg_point_id = 0
         AND tp.track_fid = t.fid
       #{sql_filters.join("")}
       #{sql_order if geo}
       #{limit_sql if !limit_sql.nil?}
     "
+    puts sql
     send_query(sql)
   end
 
