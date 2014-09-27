2ef3b4786207072e95b1b8a17bfb19a5c28f474d
Sergio Rubio <rubiojr@frameos.org>
2014-09-27 13:38:32 +0200

    Added Nominatim.reverse

--- ws/lib/nominatim.rb
+++ ws/lib/nominatim.rb
@@ -2,10 +2,19 @@ require 'open-uri'
 require 'json'
 
 class Nominatim
-  BASE_URL = "http://nominatim.openstreetmap.org/search"
+  BASE_URL = "http://nominatim.openstreetmap.org/"
   def self.hotels(place, limit = 5)
-    ret = open("#{Nominatim::BASE_URL}?q=Jaca\[hotel\]&format=json&polygon=0&addressdetails=1&limit=#{limit}").read rescue '{}'
+    ret = open("#{Nominatim::BASE_URL}/search?q=Jaca\[hotel\]&format=json&polygon=0&addressdetails=1&limit=#{limit}").read rescue '{}'
     JSON.parse(ret)
+  end
+
+  def self.reverse(lat, lon)
+    rev = nil
+    ret = open("#{BASE_URL}/reverse?lat=#{lat}&lon=#{lon}&format=json").read rescue nil
+
+    rev = JSON.parse(ret) if ret
+
+    rev
   end
 end
 
@@ -13,4 +22,6 @@ if __FILE__ == $0
   Nominatim.hotels('jaca').each do |a|
     puts "#{a['type'].capitalize}: #{a['display_name']}"
   end
+
+  puts Nominatim.reverse('44.88', '2.44')
 end
